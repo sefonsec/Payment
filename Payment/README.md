9b4c0d03cfac3f086e18533ca5d65000778306a0
Sérgio Eduardo Fonseca <sefonsec@gmail.com>
2020-01-03 13:33:45 -0300

    first commit

---
+++ Payment/README.md
@@ -0,0 +1,2 @@
+# Payment
+Exemplo de CRUD com ASP.NET Core 2.2 com Angular 8
---
+++ WebApi/Models/PaymentDetailsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class PaymentDetailsContext : DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+    }
+}
